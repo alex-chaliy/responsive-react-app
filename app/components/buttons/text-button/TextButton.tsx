6d4bf8abe334dd0e1fcc0b004b5bd9f345f3af1f
Oleksii Chalyi <alex.chaliy.42@gmail.com>
2026-01-04 21:55:06 +0200

    added gsap elastic animation on text buttons

--- app/components/buttons/text-button/TextButton.tsx
+++ app/components/buttons/text-button/TextButton.tsx
@@ -18,6 +18,12 @@ import {
   DEFAULT_TEXT_BUTTON_MODE,
 } from './TextButton.constants';
 
+// gsap imports
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/SplitText';
+
 interface TextButtonProps {
   className?: string;
   title?: string;
@@ -43,7 +49,7 @@ export default function TextButton(props: TextButtonProps) {
     linkHref = '',
     linkTarget,
     placeOnBg = DEFAULT_PLACE_ON_BG,
-    type = DEFAULT_TEXT_BUTTON_HTML_TYPE
+    type = DEFAULT_TEXT_BUTTON_HTML_TYPE,
   } = { ...props };
 
   const isLinkActive = usePathname() === linkHref;
@@ -59,15 +65,47 @@ export default function TextButton(props: TextButtonProps) {
     }
   );
 
+  const animationsContainer = useRef<HTMLSpanElement>(null);
+
+  useGSAP(
+    () => {
+      const titleSplitText = new SplitText('.rra-text-button__title', {
+        type: 'words',
+        tag: 'span',
+        wordsClass: 'rra-text-button__title-word++',
+      });
+      const titleWords = titleSplitText.words;
+      gsap.from(titleWords, {
+        // repeat: -1, // infinite animation repeat
+        yPercent: 50,
+        // xPercent: -50,
+        rotation: '-45',
+        ease: 'elastic.out',
+        duration: 1.8,
+        display: 'inline-block', // if we use tag 'span' to wrap a single character, animation doesn't work
+        // to fix that we should apply 'display: inline-block' style to every single character
+      });
+    },
+    {
+      dependencies: [title],
+      scope: animationsContainer,
+      revertOnUpdate: true,
+    }
+  );
   if (mode === 'link') {
     return (
       <>
         <Link className={classNames} href={linkHref} target={linkTarget}>
-          <span className="rra-hover-layout">
-            <span className="rra-hl-more-pseudo" />
+          <span
+            className="rra-text-button__animations-container"
+            ref={animationsContainer}
+          >
+            <span className="rra-hover-layout">
+              <span className="rra-hl-more-pseudo" />
+            </span>
+            <span className="rra-more-pseudo"></span>
+            <span className="rra-text-button__title">{title}</span>
           </span>
-          <span className="rra-more-pseudo"></span>
-          <span className="text-button-title">{title}</span>
         </Link>
       </>
     );
@@ -75,11 +113,16 @@ export default function TextButton(props: TextButtonProps) {
 
   return (
     <button className={classNames} onClick={handleClick} type={type}>
-      <span className="rra-hover-layout">
-        <span className="rra-hl-more-pseudo" />
+      <span
+        className="rra-text-button__animations-container"
+        ref={animationsContainer}
+      >
+        <span className="rra-hover-layout">
+          <span className="rra-hl-more-pseudo" />
+        </span>
+        <span className="rra-more-pseudo"></span>
+        <span className="rra-text-button__title">{title}</span>
       </span>
-      <span className="rra-more-pseudo"></span>
-      <span className="text-button-title">{title}</span>
     </button>
   );
 }
